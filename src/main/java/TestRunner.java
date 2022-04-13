b098494a995fef289a405ac7dc27d68758c0b5d3
bran3t <alex.bran3t@gmail.com>
2022-04-13 19:00:08 +0300

    Added the driver to web browser detection.

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,19 @@
+import managers.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
 public class TestRunner {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+//        WebDriver openBrowser = new ChromeDriver();
+//        openBrowser.get("https://www.google.com/");
+//        openBrowser.close();
+
+        WebDriverManager webDriverManagerObject = new WebDriverManager("Chrome");
+        System.out.println(webDriverManagerObject.getDriverVersionDetect());
+        webDriverManagerObject.getDriverVersionDetect().get("https://www.google.com/");
+        webDriverManagerObject.getDriverVersionDetect().close();
+        webDriverManagerObject.getDriverVersionDetect().quit();
+    }
 }
